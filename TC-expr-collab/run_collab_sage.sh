57e9807bdcba6b62c90348900d31e4a27e7275c5
submission2024 <145860919+submission2024@users.noreply.github.com>
2023-09-29 02:36:51 -0500

    Update run_collab_sage.sh

--- TC-expr-collab/run_collab_sage.sh
+++ TC-expr-collab/run_collab_sage.sh
@@ -1,7 +1,3 @@
-# python main.py --dataset='collab' --n_layers=3 --n_hidden=256 --encoder_lr=0.001 --predictor_lr=0.001 --runs=1 --encoder='GCN' --predictor='M-LP' --epochs=1000 --model='GCN' --save --remove_rep --gcn_denoise --train --wandb
-
-# python main.py --dataset='collab' --n_layers=3 --n_hidden=256 --encoder_lr=0.001 --predictor_lr=0.001 --runs=1 --encoder='GCN' --predictor='M-LP' --epochs=1000 --model='GCN-aug' --save --remove_rep --aug --gcn_denoise --train --wandb
-
 python main.py --dataset='collab' --n_layers=3 --n_hidden=256 --encoder_lr=0.001 --predictor_lr=0.001 --runs=2 --encoder='SAGE' --predictor='M-LP' --epochs=1000 --model='SAGE' --save --gcn_denoise --remove_rep --train --wandb
 
 python main.py --dataset='collab' --n_layers=3 --n_hidden=256 --encoder_lr=0.001 --predictor_lr=0.001 --runs=2 --encoder='SAGE' --predictor='M-LP' --epochs=1000 --model='SAGE-aug' --save --aug --gcn_denoise --remove_rep --train --wandb
