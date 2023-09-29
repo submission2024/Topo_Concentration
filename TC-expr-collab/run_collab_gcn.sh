3d91889c2183d3d15397d2b491504e0f92679a6a
submission2024 <145860919+submission2024@users.noreply.github.com>
2023-09-29 02:36:33 -0500

    Update run_collab_gcn.sh

--- TC-expr-collab/run_collab_gcn.sh
+++ TC-expr-collab/run_collab_gcn.sh
@@ -1,8 +1,3 @@
-# python main.py --dataset='collab' --n_layers=3 --n_hidden=256 --encoder_lr=0.001 --predictor_lr=0.001 --runs=2 --encoder='GCN' --predictor='M-LP' --epochs=1000 --model='GCN' --save --remove_rep --train --wandb
+python main.py --dataset='collab' --n_layers=3 --n_hidden=256 --encoder_lr=0.001 --predictor_lr=0.001 --runs=2 --encoder='GCN' --predictor='M-LP' --epochs=1000 --model='GCN' --save --remove_rep --train --wandb
 
 python main.py --dataset='collab' --n_layers=3 --n_hidden=256 --encoder_lr=0.001 --predictor_lr=0.001 --runs=2 --encoder='GCN' --predictor='M-LP' --epochs=1000 --model='GCN-aug' --save --remove_rep --aug --gcn_denoise --train --wandb
-
-# python main.py --dataset='collab' --n_layers=3 --n_hidden=256 --encoder_lr=0.001 --predictor_lr=0.001 --runs=1 --encoder='GCN' --predictor='M-LP' --epochs=1000 --model='GCN-weight' --save --wandb --gcn_denoise
-
-# python main.py --dataset='collab' --n_layers=3 --n_hidden=256 --encoder_lr=0.001 --predictor_lr=0.001 --runs=1 --encoder='GCN' --predictor='M-LP' --epochs=1000 --model='GCN-weight-aug' --save --wandb --aug --gcn_denoise
-
